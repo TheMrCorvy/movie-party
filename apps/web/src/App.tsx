96c3b475b3e87a897662a4a01555985c67ddb05c
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2025-06-06 22:07:52 -0300

    updates in the main ui

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,7 @@
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
 
 const App = () => {
 	return (
@@ -10,20 +12,51 @@ const App = () => {
 				display: "flex",
 				flexDirection: "column",
 				alignItems: "center",
-				justifyContent: "center",
+				justifyContent: "start",
+				paddingTop: "4rem",
 			}}
 		>
-			<Typography
-				variant="h4"
+			<Card
+				component="section"
 				sx={{
-					color: "#333",
-					fontWeight: "bold",
+					backgroundColor: "rgba(255, 255, 255, 0.1)",
+					backdropFilter: "blur(10px)",
+					WebkitBackdropFilter: "blur(10px)",
+					borderRadius: 12,
+					border: "1px solid rgba(255, 255, 255, 0.2)",
+					boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+					padding: 2,
+					color: "#fff",
 					textAlign: "center",
-					marginBottom: 2,
+					height: "35rem",
+					width: "59rem",
+					maxWidth: "75vw",
 				}}
 			>
-				Welcome to Movie-Party app!
-			</Typography>
+				<Box
+					sx={{
+						width: "100%",
+						height: "3rem",
+						bgcolor: "#232F3E",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "center",
+						borderRadius: 7,
+						paddingY: 2,
+					}}
+				>
+					<Typography
+						variant="h4"
+						sx={{
+							fontWeight: "bold",
+							textAlign: "center",
+						}}
+					>
+						Welcome to Movie-Party app!
+					</Typography>
+				</Box>
+			</Card>
 		</Container>
 	);
 };
