6b60e9f2635db8769977db05cfd11b7bc4ccd8a0
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-13 11:43:16 -0300

    removed comment

--- apps/movie-party-front/src/utils/setMeUp.ts
+++ apps/movie-party-front/src/utils/setMeUp.ts
@@ -26,16 +26,6 @@ const setMeUp = ({
         port: 9000,
         path: "/",
         secure: false,
-        // debug: 3, // Maximum debug level
-        // config: {
-        //     iceServers: [
-        //         { urls: "stun:stun.l.google.com:19302" },
-        //         { urls: "stun:stun1.l.google.com:19302" },
-        //         { urls: "stun:stun2.l.google.com:19302" },
-        //         { urls: "stun:stun3.l.google.com:19302" },
-        //         { urls: "stun:stun4.l.google.com:19302" },
-        //     ],
-        // },
     });
 
     peer.on("open", () => {
