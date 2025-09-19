dd066a186d2907c5237a4dd6ef030b433cf90b12
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-19 10:02:37 -0300

    implemented the Chat component

--- apps/movie-party-front/src/pages/Room.tsx
+++ apps/movie-party-front/src/pages/Room.tsx
@@ -1,7 +1,8 @@
 import { type FC } from "react";
-import { Button, Container } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 import VideoPlayerComponent from "../components/VideoPlayerComponent";
 import PeerVideo from "../components/PeerVideo";
+import Chat from "../components/Chat";
 import { useRoom } from "../hooks/useRoom";
 
 const Room: FC = () => {
@@ -23,66 +24,91 @@ const Room: FC = () => {
     return (
         <Container
             maxWidth="xl"
-            sx={{
-                height: "100vh",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                color: "white",
-            }}
+            sx={{ height: "100vh", color: "white", padding: 0 }}
         >
-            <div style={{ textAlign: "center" }}>
-                <h1>Room</h1>
-                <p>Room ID: {roomId}</p>
-
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={shareScreen}
+            <Grid container sx={{ height: "100vh" }}>
+                <Grid
+                    size={{
+                        xs: 12,
+                        md: 9,
+                    }}
+                    sx={{
+                        padding: "24px",
+                        overflowY: "auto",
+                        height: "100vh",
+                    }}
                 >
-                    Share screen
-                </Button>
+                    <div style={{ textAlign: "center" }}>
+                        <h1>Room</h1>
+                        <p>Room ID: {roomId}</p>
 
-                <div style={{ marginBottom: "20px" }}>
-                    <h3>Your Video</h3>
-                    {videoStream && (
-                        <VideoPlayerComponent stream={videoStream} />
-                    )}
-                </div>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={shareScreen}
+                        >
+                            Share screen
+                        </Button>
 
-                <div>
-                    <h3>Connected Peers: {Object.keys(peers).length}</h3>
-                    <div
-                        style={{
-                            display: "flex",
-                            flexWrap: "wrap",
-                            justifyContent: "center",
-                        }}
-                    >
-                        {peersArr.length < 1 && !ownCamera ? (
-                            <p>loading...</p>
-                        ) : (
-                            <>
-                                {ownCamera && (
-                                    <PeerVideo
-                                        key={ownCamera.peerId}
-                                        peerId={ownCamera.peerId}
-                                        stream={ownCamera.stream}
-                                    />
+                        <div style={{ marginBottom: "20px" }}>
+                            <h3>Your Video</h3>
+                            {videoStream && (
+                                <VideoPlayerComponent stream={videoStream} />
+                            )}
+                        </div>
+
+                        <div>
+                            <h3>
+                                Connected Peers: {Object.keys(peers).length}
+                            </h3>
+                            <div
+                                style={{
+                                    display: "flex",
+                                    flexWrap: "wrap",
+                                    justifyContent: "center",
+                                }}
+                            >
+                                {peersArr.length < 1 && !ownCamera ? (
+                                    <p>loading...</p>
+                                ) : (
+                                    <>
+                                        {ownCamera && (
+                                            <PeerVideo
+                                                key={ownCamera.peerId}
+                                                peerId={ownCamera.peerId}
+                                                stream={ownCamera.stream}
+                                            />
+                                        )}
+                                        {filteredPeersArr.map(
+                                            ([peerId, peer]) => (
+                                                <PeerVideo
+                                                    key={peerId}
+                                                    peerId={peerId}
+                                                    stream={peer.stream}
+                                                />
+                                            )
+                                        )}
+                                    </>
                                 )}
-                                {filteredPeersArr.map(([peerId, peer]) => (
-                                    <PeerVideo
-                                        key={peerId}
-                                        peerId={peerId}
-                                        stream={peer.stream}
-                                    />
-                                ))}
-                            </>
-                        )}
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
+                </Grid>
+                <Grid
+                    size={{
+                        xs: 12,
+                        md: 3,
+                    }}
+                    sx={{
+                        height: "100vh",
+                        display: "flex",
+                        flexDirection: "column",
+                        padding: "24px 12px",
+                    }}
+                >
+                    <Chat />
+                </Grid>
+            </Grid>
         </Container>
     );
 };
