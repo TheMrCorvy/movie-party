3e4b18e643c95e913b26e0643c61f12aaf4559a3
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-19 10:34:42 -0300

    implemented new theme

--- apps/movie-party-front/src/main.tsx
+++ apps/movie-party-front/src/main.tsx
@@ -1,19 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { RoomProvider } from "./context/RoomContextProvider";
 import Home from "./pages/Home.tsx";
 import Room from "./pages/Room.tsx";
+import theme from "./themes/theme";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter>
-            <RoomProvider>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/room/:roomId" element={<Room />} />
-                </Routes>
-            </RoomProvider>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <BrowserRouter>
+                <RoomProvider>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/room/:roomId" element={<Room />} />
+                    </Routes>
+                </RoomProvider>
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>
 );
