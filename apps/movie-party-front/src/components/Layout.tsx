271f70296d42e9b16d6aa8be5fb85f750990cd8a
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-20 11:38:37 -0300

    small bug fix

--- apps/movie-party-front/src/components/Layout.tsx
+++ apps/movie-party-front/src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
 import type { FC } from "react";
-import GlassContainer from "./GlassContainer";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 export const Layout: FC = () => {
