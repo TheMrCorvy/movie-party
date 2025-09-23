e0d9f1a4f3e226cd91b4cee35d25556eb1ac226b
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-23 10:28:30 -0300

    added password verification for new-joiners

--- apps/movie-party-back/src/rooms/roomExists.ts
+++ apps/movie-party-back/src/rooms/roomExists.ts
@@ -10,10 +10,16 @@ export interface RoomExistsParams {
 export const roomExists = ({ roomId, rooms, io }: RoomExistsParams) => {
     const room = rooms.find((r) => r.id === roomId);
     if (room) {
-        io.emit(Signals.ROOM_EXISTS, { roomExists: true });
+        io.emit(Signals.ROOM_EXISTS, {
+            roomExists: true,
+            hasPassword: room.password ? true : false,
+        });
         console.log("Room exists.");
     } else {
-        io.emit(Signals.ROOM_NOT_FOUND, { roomExists: false });
+        io.emit(Signals.ROOM_NOT_FOUND, {
+            roomExists: false,
+            hasPassword: false,
+        });
         console.log("Room doesn't exist.");
     }
 };
