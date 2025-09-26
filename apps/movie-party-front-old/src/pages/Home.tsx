dd7336029df4fb4e53f163c17ab7be8d49befd06
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-26 17:09:22 -0300

    removed old iteration of the app

--- apps/movie-party-front-old/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import EnterRoom from "../components/EnterRoom";
-
-const Home: FC = () => {
-    return <EnterRoom />;
-};
-
-export default Home;
--- apps/movie-party-front-old/src/styles/types.ts
+++
@@ -1,9 +0,0 @@
-import type { SxProps, Theme } from "@mui/material/styles";
-
-export interface Styles {
-    [key: string]: SxProps<Theme>;
-}
-
-export type ThemeOptions = "light" | "dark";
-
-export type StylesService = (theme?: ThemeOptions) => Styles;
