d92e35a0bc352ef9210cb2e60ca6cff12538c5df
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-27 19:23:01 -0300

    created logData function

---
+++ packages/shared-utils/src/logData.ts
@@ -0,0 +1,118 @@
+import { printTimeStamp } from ".";
+import { FeatureNames, isFeatureFlagEnabled } from "./featureFlags";
+
+// Copy and paste these layers into LAYERS_AVAILABLE in the .env file to enable/disable the layers
+type LayersAvailable =
+    | "messages"
+    | "room_ws"
+    | "participants_update"
+    | "camera"
+    | "camera_caller"
+    | "camera_receiver"
+    | "screen_sharing"
+    | "screen_sharing_sender"
+    | "screen_sharing_receiver"
+    | "access_user_hardware";
+
+export interface LogDataParams {
+    title?: string;
+    data?: unknown;
+    type?: "log" | "error" | "warn" | "info";
+    clearConsole?: boolean;
+    timeStamp?: boolean;
+    addSpaceAfter?: boolean;
+    layer?: LayersAvailable; // "layer" as the name implies is the layer where a certain console.log will be printed, so that all the app can have lots of console.logs but not all of them will alwaysshow up
+}
+
+export type LogData = (params: LogDataParams) => void;
+
+export const logData: LogData = ({
+    title,
+    data,
+    type = "log",
+    clearConsole = false,
+    timeStamp = false,
+    layer,
+    addSpaceAfter = false,
+}) => {
+    const layersAvailable = JSON.parse(
+        process.env.LAYERS_AVAILABLE || "[]"
+    ) as string[];
+
+    const allowAllLogs = isFeatureFlagEnabled(
+        FeatureNames.CONSOLE_LOG_ALL_LAYERS
+    );
+    const allowSpecificLayer = isFeatureFlagEnabled(
+        FeatureNames.CONSOLE_LOG_LAYER_SPECIFIC
+    );
+    let dataString: unknown;
+
+    try {
+        dataString = JSON.stringify({ data });
+    } catch (err) {
+        console.warn("The data provided was corrupted or circular.", err);
+        dataString = data;
+    }
+
+    const logLabel = title ? `${title}: ` : "Debug log: ";
+    const separator =
+        "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+
+    let logIsAvailable = false;
+
+    if (
+        layer !== undefined &&
+        layersAvailable.includes(layer) &&
+        allowSpecificLayer
+    ) {
+        logIsAvailable = true;
+        // If the layer is set and the feature flag is allowing for specific layers, then the logs are available
+    }
+
+    if (allowAllLogs) {
+        logIsAvailable = true;
+        // If layer is unset, the the developer has to set the FF CONSOLE_LOG_ALL_LAYERS in order to see any logs
+    }
+
+    if (!logIsAvailable) {
+        return;
+    }
+
+    if (clearConsole) {
+        console.clear();
+    }
+
+    switch (type) {
+        case "error":
+            console.error(logLabel, dataString);
+            break;
+
+        case "warn":
+            console.warn(logLabel, dataString);
+            break;
+
+        case "info":
+            console.info(logLabel, dataString);
+
+            break;
+
+        default:
+            console.log(logLabel, dataString);
+            break;
+    }
+
+    if (timeStamp) {
+        logSpace(addSpaceAfter);
+        console.log(printTimeStamp());
+        logSpace(addSpaceAfter);
+    }
+
+    console.log(separator);
+    logSpace(addSpaceAfter);
+};
+
+const logSpace = (addSpaceAfter: boolean) => {
+    if (addSpaceAfter) {
+        console.log(" ");
+    }
+};
