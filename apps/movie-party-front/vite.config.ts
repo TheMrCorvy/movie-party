09380ab365480e8cc321e65d1d955dc6c7cc479d
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-27 19:21:58 -0300

    added env variables support and updated package definition

--- apps/movie-party-front/vite.config.ts
+++ apps/movie-party-front/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-    plugins: [react()],
-    optimizeDeps: {
-        include: ["@mui/material", "@mui/icons-material"],
-    },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [react()],
+        optimizeDeps: {
+            include: ["@mui/material", "@mui/icons-material"],
+        },
+        define: {
+            "process.env": env,
+        },
+    };
 });
