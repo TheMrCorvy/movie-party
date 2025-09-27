e28d422fa773fcf9fe55158f9df341d40220ddb7
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-09-27 15:07:48 -0300

    implemented feature flag util

---
+++ packages/shared-utils/src/featureFlags.ts
@@ -0,0 +1,33 @@
+export enum FeatureNames {
+    "CONSOLE_LOG" = "CONSOLE_LOG",
+    "ACCESS_MICROPHONE" = "ACCESS_MICROPHONE",
+}
+
+export interface FeatureFlag {
+    enabled: boolean;
+    feature: FeatureNames;
+}
+
+const getFeatureFlags = (): FeatureFlag[] | null => {
+    const featureFlagsString = process.env.FEATURE_FLAGS;
+
+    if (!featureFlagsString) {
+        return null;
+    }
+
+    return JSON.parse(featureFlagsString);
+};
+
+export const isFeatureFlagEnabled = (ff: FeatureNames): boolean => {
+    const featureFlagList = getFeatureFlags();
+
+    if (!featureFlagList || featureFlagList.length === 0) {
+        return false;
+    }
+
+    const featureFlag = featureFlagList.find((f) => f.feature === ff);
+
+    if (!featureFlag) return false;
+
+    return featureFlag.enabled;
+};
