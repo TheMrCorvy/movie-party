30d3b1306722642f55d157cf37d6b39f0b3b8909
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-10-02 07:08:40 -0300

    updated timeStamp

--- packages/shared-utils/src/index.ts
+++ packages/shared-utils/src/index.ts
@@ -23,5 +23,5 @@ export const printTimeStamp = () => {
     const minutes = String(now.getMinutes()).padStart(2, "0");
     const seconds = String(now.getSeconds()).padStart(2, "0");
 
-    return `${year}-${month}-${day}-${hours}-${minutes}-${seconds}`;
+    return `${year}-${month}-${day} - ${hours}:${minutes}:${seconds}`;
 };
