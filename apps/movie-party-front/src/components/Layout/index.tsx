3e4f906049b71d1a3a0eb4c3893ab1d108157951
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-02 19:59:35 -0300

    added background image input on the layout

--- apps/movie-party-front/src/components/Layout/index.tsx
+++ apps/movie-party-front/src/components/Layout/index.tsx
@@ -1,17 +1,30 @@
 import { Outlet } from "react-router-dom";
 import { Box } from "@mui/material";
-import { type FC } from "react";
+import { type FC, useEffect } from "react";
 import ThemeSwitcher from "../ThemeSwitcher";
 import useLayout from "./useLayout";
+import { useBackground } from "../../context/BackgroundImageContext";
+import { BackgroundImageInput } from "../BackgroundImageInput";
 
 export const Layout: FC = () => {
+    const { background } = useBackground();
+
     useLayout({
         pageIsRoom: window.location.pathname.split("/")[1] === "room",
     });
 
+    useEffect(() => {
+        document.body.style.backgroundImage = `url(${background})`;
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.body.style.height = "100vh";
+    }, [background]);
+
     return (
         <Box sx={{ display: "flex", flexDirection: "column" }}>
             <ThemeSwitcher />
+            <BackgroundImageInput />
             <Outlet />
         </Box>
     );
