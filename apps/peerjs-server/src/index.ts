f947143465ad9a0da8f05c39b09a9d58cab8883f
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-11 08:03:07 -0300

    implemented new version of peerjs server

--- apps/peerjs-server/src/index.ts
+++ apps/peerjs-server/src/index.ts
@@ -1,21 +1,53 @@
-import { PeerServer } from "peer";
+import dotenv from "dotenv";
+import { logData } from "@repo/shared-utils/log-data";
 
-const PORT = process.env.PEERJS_PORT ? parseInt(process.env.PEERJS_PORT) : 9000;
+dotenv.config();
 
-const peerServer = PeerServer({
-    port: PORT,
-    path: "/",
-});
+const initServer = async () => {
+    const { PeerServer } = await import("peer");
 
-peerServer.on("connection", (client) => {
-    console.log(`Peer connected: ${client.getId()}`);
-});
+    const PORT = process.env.PEERJS_PORT
+        ? parseInt(process.env.PEERJS_PORT)
+        : 9000;
+    const peerjsHost =
+        process.env.NODE_ENV === "dev"
+            ? "http://localhost:" + PORT
+            : process.env.PEERJS_BASE_URL;
 
-peerServer.on("disconnect", (client) => {
-    console.log(`Peer disconnected: ${client.getId()}`);
-});
+    const peerServer = PeerServer({
+        port: PORT,
+        path: "/",
+    });
 
-console.log(`🚀 PeerJS server is running on port ${PORT}`);
-console.log(
-    `✅ Verify that the server is running by visiting: http://localhost:${PORT}`
-);
+    peerServer.on("connection", (client) => {
+        logData({
+            title: `Peer connected: ${client.getId()}`,
+            data: client,
+            addSpaceAfter: true,
+            timeStamp: true,
+            layer: "camera",
+        });
+    });
+
+    peerServer.on("disconnect", (client) => {
+        logData({
+            title: `Peer disconnected: ${client.getId()}`,
+            data: client,
+            addSpaceAfter: true,
+            timeStamp: true,
+            layer: "camera",
+        });
+    });
+
+    logData({
+        title: `🚀 PeerJS server is running on port ${PORT}`,
+        timeStamp: true,
+        addSpaceAfter: true,
+        layer: "*",
+        data: {
+            port: `✅ Verify that the server is running by visiting: ${peerjsHost}`,
+        },
+    });
+};
+
+initServer();
