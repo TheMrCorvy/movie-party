93e853e6be75a78e4bc6027702ee2cdf0a2aaf1b
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-11 05:55:56 -0300

    implemented lazy loading in the home page

--- apps/movie-party-front/src/pages/Home.tsx
+++ apps/movie-party-front/src/pages/Home.tsx
@@ -1,8 +1,50 @@
-import { type FC } from "react";
-import CreateRoom from "../components/CreateRoom";
+import { lazy, Suspense, type FC } from "react";
+import { Skeleton, Container, Box } from "@mui/material";
+
+const CreateRoom = lazy(() => import("../components/CreateRoom"));
+
+const CreateRoomSkeleton = () => (
+    <Box
+        sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "100vh",
+            width: "100%",
+        }}
+    >
+        <Container maxWidth="sm">
+            <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                <Skeleton
+                    variant="text"
+                    width="60%"
+                    height={40}
+                    sx={{ mb: 2 }}
+                />
+                <Skeleton
+                    variant="rounded"
+                    width="100%"
+                    height={56}
+                    sx={{ mb: 2 }}
+                />
+                <Skeleton
+                    variant="rounded"
+                    width="100%"
+                    height={56}
+                    sx={{ mb: 2 }}
+                />
+                <Skeleton variant="rounded" width="100%" height={48} />
+            </Box>
+        </Container>
+    </Box>
+);
 
 const Home: FC = () => {
-    return <CreateRoom />;
+    return (
+        <Suspense fallback={<CreateRoomSkeleton />}>
+            <CreateRoom />
+        </Suspense>
+    );
 };
 
 export default Home;
