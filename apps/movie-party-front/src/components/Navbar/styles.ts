1af2d82b90cbda1159c69b210ba700351a1a76c2
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-15 10:41:42 -0300

    created the styles for the Navbar

---
+++ apps/movie-party-front/src/components/Navbar/styles.ts
@@ -0,0 +1,46 @@
+import {
+    backdropFilter,
+    borderWhite,
+    lightThemeBtnBg,
+    lightThemeLighterBg,
+} from "../../styles/components";
+import type { StylesService } from "../../styles/types";
+
+const styles: StylesService = () => {
+    return {
+        fabButtons: {
+            backgroundColor: lightThemeBtnBg,
+            "&:hover": {
+                backgroundColor: lightThemeLighterBg,
+            },
+            color: "white",
+            backdropFilter: backdropFilter,
+            WebkitBackdropFilter: backdropFilter,
+            boxShadow: "none",
+            border: borderWhite,
+        },
+        fabButtonsContainer: {
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+            display: "flex",
+            gap: 3,
+        },
+        endCallStyles: {
+            color: "white",
+            backdropFilter: backdropFilter,
+            WebkitBackdropFilter: backdropFilter,
+            border: borderWhite,
+            boxShadow: "none",
+            backgroundColor: "rgba(255, 61, 0, 0.30)",
+            "&:hover": {
+                backgroundColor: "rgba(255, 61, 0, 0.40)",
+            },
+        },
+        spacer: {
+            flexGrow: 1,
+        },
+    };
+};
+
+export default styles;
