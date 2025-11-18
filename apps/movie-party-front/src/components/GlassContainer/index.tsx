89c9565c9540ab5fdbb351675c2aac19960c7e76
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-18 11:56:24 -0300

    now the container supports more porps and can set the primitive component

--- apps/movie-party-front/src/components/GlassContainer/index.tsx
+++ apps/movie-party-front/src/components/GlassContainer/index.tsx
@@ -1,8 +1,8 @@
 import { Box, useTheme } from "@mui/material";
-import type { FC } from "react";
+import type { ElementType } from "react";
 import styles from "./styles";
 
-export interface GlassContainerProps {
+type GlassContainerProps<T extends ElementType = "section"> = {
     height?: string | number;
     width?: string | number;
     direction?:
@@ -18,31 +18,39 @@ export interface GlassContainerProps {
         | "unset";
     gap?: number | string;
     children: React.ReactNode;
-}
+    justifyContent?: "start" | "end" | "space-between" | "space-around";
+    primitive?: T;
+} & React.ComponentPropsWithoutRef<T>;
 
-const GlassContainer: FC<GlassContainerProps> = ({
+function GlassContainer<T extends ElementType = "section">({
     children,
     height,
     width,
     direction = "column",
     gap = 2,
-}) => {
+    justifyContent = "start" as const,
+    primitive = "section" as T,
+    ...restProps
+}: GlassContainerProps<T>) {
     const theme = useTheme();
     const { containerStyles } = styles(theme.palette.mode);
 
     return (
         <Box
+            component={primitive}
             sx={{
                 ...containerStyles,
                 height: height || "fit-content",
                 width: width || "fit-content",
                 flexDirection: direction,
                 gap,
+                justifyContent: justifyContent as any,
             }}
+            {...(restProps as any)}
         >
             {children}
         </Box>
     );
-};
+}
 
 export default GlassContainer;
