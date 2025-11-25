e8998a2ab9414f53d9aa0258fa448faf1ae9041a
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-25 06:41:58 -0300

    now the screen player container is not rendered on mobile

--- apps/movie-party-front/src/pages/Room.tsx
+++ apps/movie-party-front/src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { Suspense, type FC } from "react";
+import { lazy, Suspense, type FC } from "react";
 import { Container, Grid, Skeleton } from "@mui/material";
 import GlassContainer from "../components/GlassContainer";
 import styles from "../styles/roomPageStyles";
@@ -7,7 +7,7 @@ import Navbar from "../components/Navbar";
 import { useMediaQuery } from "../hooks/useMediaQuery";
 import ScreenPlayer from "../components/ScreenPlayer";
 import Chat from "../components/Chat";
-import PeerVideo from "../components/PeerVideo";
+const PeerVideo = lazy(() => import("../components/PeerVideo"));
 
 const Room: FC = () => {
     const {
@@ -33,35 +33,70 @@ const Room: FC = () => {
                         sx={gridColFlex}
                     >
                         <GlassContainer width={"100%"}>
-                            <GlassContainer
-                                height={"auto"}
-                                width={"100%"}
-                                direction="row"
-                            >
-                                <>
-                                    {room.participants.map((participant) => (
-                                        <Suspense
-                                            key={`peer-video-${participant.id}`}
-                                            fallback={
-                                                <Skeleton
-                                                    variant="rounded"
-                                                    width="100%"
-                                                    height={300}
-                                                    sx={{ borderRadius: 2 }}
-                                                />
-                                            }
-                                        >
-                                            <PeerVideo
-                                                peerName={participant.name}
-                                                stream={participant.stream}
-                                                isMyCamera={
-                                                    participant.id === room.myId
-                                                }
+                            {isLgUp ? (
+                                <GlassContainer
+                                    height={"auto"}
+                                    width={"100%"}
+                                    direction="row"
+                                >
+                                    <>
+                                        {room.participants.map(
+                                            (participant) => (
+                                                <Suspense
+                                                    key={`peer-video-${participant.id}`}
+                                                    fallback={
+                                                        <Skeleton
+                                                            variant="rounded"
+                                                            width="100%"
+                                                            height={300}
+                                                            sx={{
+                                                                borderRadius: 2,
+                                                            }}
+                                                        />
+                                                    }
+                                                >
+                                                    <PeerVideo
+                                                        peerName={
+                                                            participant.name
+                                                        }
+                                                        stream={
+                                                            participant.stream
+                                                        }
+                                                        isMyCamera={
+                                                            participant.id ===
+                                                            room.myId
+                                                        }
+                                                    />
+                                                </Suspense>
+                                            )
+                                        )}
+                                    </>
+                                </GlassContainer>
+                            ) : (
+                                room.participants.map((participant) => (
+                                    <Suspense
+                                        key={`peer-video-${participant.id}`}
+                                        fallback={
+                                            <Skeleton
+                                                variant="rounded"
+                                                width="100%"
+                                                height={300}
+                                                sx={{
+                                                    borderRadius: 2,
+                                                }}
                                             />
-                                        </Suspense>
-                                    ))}
-                                </>
-                            </GlassContainer>
+                                        }
+                                    >
+                                        <PeerVideo
+                                            peerName={participant.name}
+                                            stream={participant.stream}
+                                            isMyCamera={
+                                                participant.id === room.myId
+                                            }
+                                        />
+                                    </Suspense>
+                                ))
+                            )}
                             {isLgUp && (
                                 <ScreenPlayer
                                     remoteScreen={remoteScreen}
