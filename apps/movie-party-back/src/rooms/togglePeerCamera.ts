d3ec4bb0353b9816f7490c0e8fea5eb831201231
Gonzalo Corvalan <salvatore.hakase@gmail.com>
2025-11-28 06:05:24 -0300

    added more verifications for toggle camera signal

--- apps/movie-party-back/src/rooms/togglePeerCamera.ts
+++ apps/movie-party-back/src/rooms/togglePeerCamera.ts
@@ -1,13 +1,16 @@
 import { logData } from "@repo/shared-utils/log-data";
 import {
+    ServerRoom,
     Signals,
     ToggleCameraWsCallbackParams,
     ToggleCameraWsParams,
 } from "@repo/type-definitions/rooms";
 import { Server as SocketIOServer } from "socket.io";
+import roomValidation from "../utils/roomValidations";
 
 export interface TogglePeerCameraParams extends ToggleCameraWsParams {
     io: SocketIOServer;
+    rooms: ServerRoom[];
 }
 
 export type TogglePeerCamera = (params: TogglePeerCameraParams) => void;
@@ -17,9 +20,28 @@ export const togglePeerCamera: TogglePeerCamera = ({
     peerId,
     cameraStatus,
     io,
+    rooms,
 }) => {
     if (!roomId) {
         io.emit(Signals.ROOM_NOT_FOUND);
+        return;
+    }
+
+    const { room, roomIndex, roomExists, peerIsParticipant, peer } =
+        roomValidation({
+            rooms,
+            roomId,
+            peerId,
+            peerShouldBeParticipant: true,
+        });
+
+    if (!room || roomIndex === -1 || !roomExists) {
+        io.emit(Signals.ROOM_NOT_FOUND);
+        return;
+    }
+
+    if (!peer || !peerIsParticipant) {
+        io.emit(Signals.ERROR);
         return;
     }
 
