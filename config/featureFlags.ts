ba3e59d1fc1e75cc4733fd14be0dcf2dbc727a89
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-06-14 07:47:39 -0300

    added configuration for logData and FeatureFlags

--- config/featureFlags.ts
+++ config/featureFlags.ts
@@ -3,6 +3,10 @@ import { FeatureNames } from "@salvatore.hakase/log-data";
 
 export const FeatureFlagsAvailable = {
     ...FeatureNames,
+    CONSOLE_LOG_ALL_LAYERS: "CONSOLE_LOG_ALL_LAYERS",
+    CONSOLE_LOG_LAYER_SPECIFIC: "CONSOLE_LOG_LAYER_SPECIFIC",
+    ACCESS_MICROPHONE: "ACCESS_MICROPHONE",
+    PLAY_SOUNDS: "PLAY_SOUNDS",
 } as const;
 
 export type FeatureFlagsAvailable =
--- config/log-data.ts
+++ config/log-data.ts
@@ -3,5 +3,16 @@ import "@salvatore.hakase/log-data";
 declare module "@salvatore.hakase/log-data" {
     export interface LayersAvailable {
         strapi_service: "strapi_service";
+        messages: "messages";
+        room_ws: "room_ws";
+        participants_update: "participants_update";
+        camera: "camera";
+        camera_caller: "camera_caller";
+        camera_receiver: "camera_receiver";
+        screen_sharing: "screen_sharing";
+        screen_sharing_sender: "screen_sharing_sender";
+        screen_sharing_receiver: "screen_sharing_receiver";
+        access_user_hardware: "access_user_hardware";
+        poll: "poll";
     }
 }
