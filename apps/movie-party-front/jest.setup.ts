5015b9250f6bf53685bdf2dfa2e0ade3da291a34
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2026-08-16 11:15:09 -0300

    updated tests suite

--- apps/movie-party-front/jest.setup.ts
+++ apps/movie-party-front/jest.setup.ts
@@ -1,6 +1,14 @@
 import "@testing-library/jest-dom";
 
 import { TextEncoder, TextDecoder } from "util";
+import { hgts } from "@salvatore.hakase/hgts";
+import { resources } from "./src/lib/HGTS";
 
 global.TextEncoder = TextEncoder as any;
 global.TextDecoder = TextDecoder as typeof global.TextDecoder;
+
+hgts.setup({
+    resources,
+    defaultLocale: "es",
+    fallbackLocale: "en",
+});
